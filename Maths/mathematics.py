db929cb21e50bffdce76b94450bcbe7170d53bc3
NRSKhouw <nickr.setiawan@gmail.com>
2025-01-01 08:19:53 -0500

    add basic functions

---
+++ Maths/mathematics.py
@@ -0,0 +1,20 @@
+def summation(a, b):
+    '''
+    Add function summation which takes in the a and b as a number arguments
+    '''
+    result = a + b
+    return result
+
+def subtraction(a, b):
+    '''
+    Add function subtraction which takes in the a and b as a number arguments
+    '''
+    result = a - b
+    return result
+
+def multiplication(a, b):
+    '''
+    Add function multiplication which takes in the a and b as a number arguments
+    '''
+    result = a * b
+    return result
